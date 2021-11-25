0dd14ee60d31682b9779cdf45542c3e7770421ce
Edmond Szeto <eszeto831@gmail.com>
2021-11-24 15:37:44 -1000

    delay rpc test

--- Assets/Scripts/RpcTest.cs
+++ Assets/Scripts/RpcTest.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 
 public class RpcTest : NetworkBehaviour
 {
+
     public override void OnNetworkSpawn()
     {
         if (IsClient)
         {
             TestServerRpc(0);
+            TestDelayServerRpc(1000);
         }
     }
 
@@ -26,5 +28,26 @@ public class RpcTest : NetworkBehaviour
     {
         Debug.Log("Server Received the RPC #" + value);
         TestClientRpc(value);
+    }
+
+    [ClientRpc]
+    void TestDelayClientRpc(int value)
+    {
+        if (IsClient)
+        {
+            Debug.Log("!!!Client Received the Delay RPC #" + value);
+            TestDelayServerRpc(value + 1);
+        }
+    }
+
+    [ServerRpc]
+    void TestDelayServerRpc(int value)
+    {
+        Debug.Log("!!!Server Received the Delay RPC #" + value);
+        for(var i=0; i<10000; i++)
+        {
+            Debug.Log("-Delay for #" + value);
+        }
+        TestDelayClientRpc(value + 1);
     }
 }
